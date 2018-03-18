d3692d047da8899bf1ffb4e726481602c28d55be
lomassubedi <subedilomas@gmail.com>
2018-03-18 13:35:24 +0545

    seperate htm file, sd.

--- esp32_web_serv/src/main.cpp
+++ esp32_web_serv/src/main.cpp
@@ -4,143 +4,41 @@
  * Controller : ESP WROOM - 32
  */
 
-#include <WiFiClient.h>
-#include <ESP32WebServer.h>
 #include <WiFi.h>
-#include <ESPmDNS.h>
-
-#include "FS.h"
+#include <WiFiClient.h>
 #include "SD.h"
-#include "SPI.h"
-
 
 const char* ssid = "internets";
 const char* password = "CLFA4ABD38";
 
-ESP32WebServer server ( 80 );
-
 const int led = 13;
 
-/* this array contains the web will be responded to client
-it uses jquery for making GET request and processing slider UI control */
-char res[2048]=
-"<!DOCTYPE html>\
-<html>\
-<head>\
-<title>Chamber Control</title>\
-</head>\
-<style> \
-  header, footer {\
-    padding: 1em;\
-    color: white;\
-    background-color: green;\
-    clear: left;\
-    text-align: center;\
-  }\
-  nav {   \
-    float: left;\
-    border-right: thik soli #ff0000;\
-    max-width: 160px;\
-    margin: 20px;\
-    padding: 1em;\
-  }\
-  .controlTmpHum, .readTempHum {\
-    float: center\
-      padding: 1em;\
-      overflow: hidden;   \
-  }   \
-</style>\
-<body>\
-  <header><h1> Meat Curing Chamber Dashboard </h1></header>\
-  <div class='enableDisableDevice'>\
-    <nav>\
-      <form id='checkDev'>Device Control<br>\
-        <input type='checkbox' name='freezer' value='0'>Freezer<br>\
-        <input type='checkbox' name='humidifier' value='0'>Humidifier<br>\
-        <input type='checkbox' name='deHumidifier' value='0'>Dehumidifier<br>\
-        <input type='checkbox' name='heater' value='0'>Heater<br>\
-        <input type='checkbox' name='internalFan' value='0'>Internal Fan<br>\
-        <input type='checkbox' name='freshAirFan' value='0'>Fresh air fan<br>\
-        <input type='checkbox' name='dev7' value='0'>Device 7<br>\
-        <input type='checkbox' name='dev8' value='0'>Device 8<br>\
-      </form>\
-    </nav>\
-  </div>\
-  <div class='controlTmpHum'>\
-    <br>\
-    <form'>\
-      Set Humidity: <input type='text' name='setHumidity'>\
-      <br>\
-      <br>\
-      Set Temperature: <input type='text' name='setTemperature'>\
-      <br>\
-      <br>\
-      <input type='submit' value='Save'>\
-    </form>\
-  </div>\
-  <div class='readHum'>\
-    <br>\
-      Current Humidity: \
-      <script type='text/javascript'>\
-      document.write('100')\
-      </script> % </p> \
-  </div>\
-  <div class='readTemp'>\
-    <p>Current Temperature: \
-    <script type='text/javascript'>\
-      document.write('23')\
-      </script> &degF</p> \
-  </div>\
-  <footer>\
-    <p>\
-      Copyright &copy <i>Keyl Schaub, 2018</i>\
-    </p>\
-  </footer>\
-</body>\
-</html>";
+// Set web server port number to 80
+WiFiServer server(80);
 
-void handleRoot() {
-  server.send(200, "text/html", res);
-}
+// Web page file stored on the SD card
+File webFile; 
 
-void handleNotFound(){
-  String message = "File Not Found\n\n";
-  server.send(404, "text/plain", message);
-}
-
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
-
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.name(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("  SIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
-}
+// Variable to store the HTTP request
+String header;
 
 void setup() {
+
   Serial.begin(115200);
+
+  // Setup SD card
+  if(!SD.begin()){
+      Serial.println("Card Mount Failed");
+      return;
+  }
+
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+      Serial.println("No SD card attached");
+      return;
+  }
+
   WiFi.begin(ssid, password);
   Serial.println("");
   
@@ -156,33 +54,70 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Setup SD card
-
-  if(!SD.begin()){
-      Serial.println("Card Mount Failed");
-      return;
-  }
-
-  uint8_t cardType = SD.cardType();
-
-  if(cardType == CARD_NONE){
-      Serial.println("No SD card attached");
-      return;
-  }
-
-  listDir(SD, "/", 0);
-
-  if (MDNS.begin("esp32")) {
-  Serial.println("MDNS responder started");
-  }
-  
-  server.on("/", handleRoot);
-  
-  server.onNotFound(handleNotFound);
   server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop() {
-  server.handleClient();
-}
+   WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {  // if new client connects
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {   // client data available to read
+        char c = client.read(); // read 1 byte (character) from client
+        header += c;
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          // Send XML file or Web page
+          // If client already on the web page, browser requests with AJAX the latest
+          // sensor readings (ESP32 sends the XML file)
+          if (header.indexOf("update_readings") >= 0) {
+            // send rest of HTTP header
+            client.println("Content-Type: text/xml");
+            client.println("Connection: keep-alive");
+            client.println();
+            // Send XML file with sensor readings
+            // sendXMLFile(client);
+          }
+          // When the client connects for the first time, send it the index.html file
+          // stored in the microSD card
+          else {  
+            // send rest of HTTP header
+            client.println("Content-Type: text/html");
+            client.println("Connection: keep-alive");
+            client.println();
+            // send web page stored in microSD card
+            webFile = SD.open("/dashboard.html");
+            if (webFile) {
+              while(webFile.available()) {
+                // send web page to client
+                client.write(webFile.read()); 
+              }
+              webFile.close();
+            }
+          }
+          break;
+        }
+        // every line of text received from the client ends with \r\n
+        if (c == '\n') {
+          // last character on line of received text
+          // starting new line with next character read
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // a text character was received from client
+          currentLineIsBlank = false;
+        }
+        } // end if (client.available())
+    } // end while (client.connected())
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+  } // end if (client)
+}   
+
